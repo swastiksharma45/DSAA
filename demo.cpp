97291d1a40f4d62079b9b7c2c3f141e78cb951c9
swastik sharma <swastiksharma9010@gmail.com>
2026-07-18 22:55:24 +0530

    .

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+/*To Run the code Press ctrl+shift+B */
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+} 
