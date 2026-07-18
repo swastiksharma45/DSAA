2a97c9e59309f7cf0c7d7067200a3bba81f6f50f
swastik sharma <swastiksharma9010@gmail.com>
2026-07-18 23:43:31 +0530

    right angle star triangle using functions

--- demo.cpp
+++ demo.cpp
@@ -2,13 +2,17 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
+void print(int n){
     for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
+        for(int j=0;j<i;j++)
+        {
             cout<<"*";
         }
         cout<<endl;
     }
-} 
+}
+    int main(){
+        int n;
+        cin>>n;
+        print(n);
+    }
