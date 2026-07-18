dd76a13d164afe8d1ca9d1fea5393914f6c37fea
swastik sharma <swastiksharma9010@gmail.com>
2026-07-18 23:55:33 +0530

    more optimised version for printing row number same time as number of row in right angle triangle

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,8 @@
 #include<math.h>
 using namespace std;
 void print(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++)
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
         {
             cout<<i;
         }
