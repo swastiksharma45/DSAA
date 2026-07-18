d505e5ce3d3d05bda97b3e381741ade9c486292a
swastik sharma <swastiksharma9010@gmail.com>
2026-07-18 23:57:59 +0530

    printing lower star triangle in more optimised way using functins

--- demo.cpp
+++ demo.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 void print(int n){
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++)
+        for(int j=i;j<=n;j++)
         {
-            cout<<i;
+            cout<<"*";
         }
         cout<<endl;
     }
