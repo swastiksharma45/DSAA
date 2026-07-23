46a982eb64ef30c782e93e4c7809d52dd5a5257e
swastik sharma <swastiksharma9010@gmail.com>
2026-07-23 23:30:32 +0530

    star triangle

--- demo.cpp
+++ demo.cpp
@@ -3,16 +3,18 @@
 #include<math.h>
 using namespace std;
 void print(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=i;j<=n;j++)
-        {
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
             cout<<"*";
         }
         cout<<endl;
     }
 }
-    int main(){
-        int n;
-        cin>>n;
-        print(n);
-    }
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+}
