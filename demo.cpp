2c6a9de2fe8396561c160c30245452e559df940c
swastik sharma <swastiksharma9010@gmail.com>
2026-07-25 16:56:10 +0530

    star triangle downside

--- demo.cpp
+++ demo.cpp
@@ -2,13 +2,15 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-void print(int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            cout<<" ";
+
+void print(int n)
+{
+    for(int i=-1;i<n;i++){
+        for(int j=0;j<=i;j++){
+                cout<<" ";
         }
-        for(int j=0;j<2*i+1;j++){
-            cout<<"*";
+        for(int j=i;j<=n-i;j++){
+                cout<<"*";
         }
         cout<<endl;
     }
