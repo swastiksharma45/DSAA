054d258473cce52af1578c16dd1a802878d21da1
swastik sharma <swastiksharma9010@gmail.com>
2026-07-25 23:25:29 +0530

    continuous no. triangle

--- demo.cpp
+++ demo.cpp
@@ -3,15 +3,11 @@
 #include<math.h>
 using namespace std;
  void print(int n){
-    for(int i=0;i<n;i++){
-        for(int j=1;j<=i+1;j++){
-            cout<<j;
-        }
-        for(int j=1;j<2*(n-i)-1;j++){
-            cout<<" ";
-        }
-        for(int j=i+1;j>=1;j--){
-            cout<<j;
+    int num=1;
+    for(int i=1;i<=n+1;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num=num+1;
         }
         cout<<endl;
     }
