eb5aa2fb070f80a3edf2b44d7411185a484ced45
swastik sharma <swastiksharma9010@gmail.com>
2026-07-26 23:39:38 +0530

    array

--- demo.cpp
+++ demo.cpp
@@ -2,18 +2,8 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
- void print(int n){
-        for(int i=0;i<n;i++){
-            for(char ch='A';ch<='A'+i;ch++){
-                cout<<ch;
-            }
-            cout<<endl;
-        }
-}
- 
-int main() {
-    int n;
-    cin >> n;
-    print(n);
+int main(){
+    int year[]={2021,2022,2023};
+    cout<<year[0]<<endl;
 }
 
