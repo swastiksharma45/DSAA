49786cd57a5b9e0fa2abb748f2a98a1ec17a40b3
swastik sharma <swastiksharma9010@gmail.com>
2026-08-08 23:25:56 +0530

    Wrong Selection Sort pt1

--- demo.cpp
+++ demo.cpp
@@ -2,9 +2,17 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-int main(){
-    string car[]={"Corvette","Ferrari","Audi"};
-    cout<<car[0]<<endl;
-    return 0;
+vector<int> selectionSort(vector<int>& nums){
+    int n=nums.size();
+    for(int i=0;i<n-1;i++){
+        int minIndex=i;
+        for(int j=i;j<n;j++){
+            if(nums[j]<nums[minIndex]){
+                minIndex=j;
+            } 
+        }
+        swap(nums[i],nums[minIndex]);
+    }
+    return nums;
 }
 
